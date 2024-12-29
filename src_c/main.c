5e2fc705230a7df66737c248d3da932d43f13d46
James <21203355+jamesb-jay@users.noreply.github.com>
2024-12-29 23:19:56 +0000

    add day1 C implementation

--- src_c/main.c
+++ src_c/main.c
@@ -5,19 +5,97 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #define DOFATAL(message) do {printf("Fatal: %s\n", message); return 1;} while (0)
 
+static int cmpInt(const void *a, const void *b)
+{
+	return (*(int*)a - *(int*)b);
+}
 
 static void day1(FILE *fp)
 {
-	(void) fp;
+	printf("\tReading file...\n");
+
+	rewind(fp);
+
+	size_t list_length = 0;
+	size_t list_capacity = 0xF00;
+	int *left_list  = calloc(list_capacity, sizeof(int));
+	int *right_list = calloc(list_capacity, sizeof(int));
+	char line_buffer[256];
+
+	if (left_list == NULL || right_list == NULL)
+	{
+		printf("Failed to allocate memory!");
+		return;
+	}
+
+	while (fgets(line_buffer, sizeof line_buffer, fp) != NULL)
+	{
+		char *tok = strtok(line_buffer, " ");
+		int l = atoi(tok);
+		tok = strtok(NULL, " ");
+		int r = atoi(tok);
+
+		left_list [list_length] = l;
+		right_list[list_length] = r;
+		list_length++;
+
+		if (list_length >= list_capacity)
+		{
+			list_capacity += 0xF00;
+			left_list  = realloc(left_list,  sizeof(int) * list_capacity);
+			right_list = realloc(right_list, sizeof(int) * list_capacity);
+		}
+	}
+
+	/* If fgets failed for a reason other than EOF */
+	if (!feof(fp))
+	{
+		printf("Failed to read file!\n");
+		return;
+	}
+
+	printf("\tSorting lists...\n");
+	qsort(left_list, list_length, sizeof(int), cmpInt);
+	qsort(right_list, list_length, sizeof(int), cmpInt);
+
+	printf("\tSumming difference...\n");
+	unsigned int total_diff = 0;
+	for (size_t i = 0; i < list_length; i++)
+	{
+		total_diff += abs(left_list[i] - right_list[i]);
+	}
+
+	printf("\tTotal list difference = %u\n", total_diff);
+
+	printf("\tFinding similarity score...\n");
+
+	unsigned int similarity_score = 0;
+	for (size_t i = 0; i < list_length; i++)
+	{
+		int appearances = 0;
+		for (size_t j = 0; j < list_length; j++)
+		{
+			if (left_list[i] == right_list[j]) appearances++;
+		}
+
+		similarity_score += appearances * left_list[i];
+	}
+
+	printf("\tSimilarity score = %u\n", similarity_score);
+
+	free(left_list);
+	free(right_list);
 }
 
 
 int main(int argc, const char **argv)
 {
+	/* Arg guarding */
 	if (argc < 2) DOFATAL("No puzzle input!");
 	if (argc < 3) DOFATAL("No day!");
 
@@ -38,8 +116,8 @@ int main(int argc, const char **argv)
 
 	if (!fp) DOFATAL("Could not read puzzle input!");
 
+	/* Run day */
 	printf("==== Starting AOC day %d\n", day);
-
 
 	switch (day)
 	{
